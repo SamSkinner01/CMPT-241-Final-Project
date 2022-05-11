874eb473944334cadb16c3e7f9e2b2a167945511
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2022-05-11 19:47:01 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+
+</head>
+
+<body>
+	<h1> Contact us using the following platforms: </h1>
+	  
+	  <p><strong><a href = "https://www.gmail.com"> Email </a>: </strong> <em> reddit2.0@gmail.com </em></p>
+	   <img src="images/email.png" alt="Gmail">
+	   
+	  <p><strong> <a href = "https://www.instagram.com"> Instagram </a>:</strong> <em> Reddit 2.0 </em> </p>
+	  <img src="images/Instagram_logo.png" alt="Gmail">
+	  
+	  <p><strong> <a href = "https://www.twitter.com"> Twitter</a>:</strong> <em> Reddit 2.0  </em>  </p>
+	  <img src="images/twitter.png" alt="Gmail">
+	  
+	  <p><strong> <a href = "https://www.facebook.com"> Facebook </a>: </strong> <em> Reddit 2.0  </em>  </p>
+	  <img src="images/fb.png" alt="Gmail">
+	  
+</body>
+
+</html>
